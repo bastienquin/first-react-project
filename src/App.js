743afb3a0dd5aee68f25cf2a9e509f32dc205323
Bastien <bastien.quin@hotmail.com>
2021-11-16 21:12:07 +0100

    [+] Adding play/pause button

--- src/App.js
+++ src/App.js
@@ -6,29 +6,54 @@ class Incrementer extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
-        this.timer = null;
+        this.state = {
+            date: new Date(),
+            timer: null
+        };
     }
 
     componentDidMount() {
-        this.timer = setInterval(this.increment.bind(this), 1000);
+        this.play();
     }
 
     componentWillUnmount() {
-        clearInterval(this.timer);
+        window.clearInterval(this.state.timer);
     }
 
     increment() {
-        this.setState((state, props) => {
-            return {date: new Date()};
-        });
+        this.setState({date: new Date()});
+
     }
+
+    play() {
+        window.clearInterval(this.state.timer);
+        this.setState({timer: window.setInterval(this.increment.bind(this), 1000)});
+    }
+
+    pause() {
+        window.clearInterval(this.state.timer);
+        this.setState({timer: null});
+    }
+
+    toggleAction() {
+        this.state.timer ? this.pause() : this.play();
+    }
+
+    labelButton() {
+        return this.state.timer ? 'Pause' : 'Play';
+    }
+
+    classButton() {
+        return this.state.timer ? 'pause' : 'play';
+    }
+
 
     render() {
         return (
             <div>
                 <h1>{this.state.date.toLocaleDateString()}</h1>
                 <h2>{this.state.date.toLocaleTimeString()}</h2>
+                <button onClick={this.toggleAction.bind(this)} className={"btn-" + this.classButton()}>{this.labelButton()}</button>
             </div>
         );
     }
