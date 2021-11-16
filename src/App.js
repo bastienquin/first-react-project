f256c86b33699be1c32269ae6a9942a7f5817f4c
Bastien <bastien.quin@hotmail.com>
2021-11-16 20:10:23 +0100

    [+] First auto-increment counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Incrementer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        };
+        this.timer = null;
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(this.increment.bind(this), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    increment() {
+        this.setState((state, props) => {
+            return {counter: state.counter + 1};
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.counter}</h1>
+            </div>
+        );
+    }
+
+}
 
 function App() {
 
@@ -7,7 +42,8 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Edit <code>src/App.js</code> and save to reload.</p>
+                <p>Hello World !</p>
+                <Incrementer />
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
             </header>
         </div>
